1162d014b5abd370bdd795e760f4211d5baceda8
Gautamverma2003 <gautam146official@gmail.com>
2025-02-05 13:56:21 +0530

    Input stream reader

--- Day_4/FileReaderAndBuffferReader/ReadUserInput.java
+++ Day_4/FileReaderAndBuffferReader/ReadUserInput.java
@@ -1,14 +1,27 @@
-import java.io.FileInputStream;
+
+
+import java.io.BufferedReader;
+import java.io.FileWriter;
 import java.io.IOException;
-import java.io.InputStream;
 import java.io.InputStreamReader;
-import java.util.Scanner;
+import java.nio.charset.StandardCharsets;
 
 public class ReadUserInput {
     public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
-        System.out.println("enter a string");
-        String str = sc.nextLine();
-        InputStreamReader isr = new  InputStreamReader(str);
+        String filePath = "C:/Gautam/Week-3/Day_4/FileReaderAndBuffferReader/newFile.txt/";
+
+        try(BufferedReader br = new BufferedReader(new InputStreamReader(System.in, StandardCharsets.UTF_8));
+            FileWriter fw = new FileWriter(filePath, true);
+        ) {
+            String line;
+            System.out.println("Enetr text to write to the file and type exit to exit");
+            while (!(line = br.readLine()).equalsIgnoreCase("exit")){
+                fw.write(line+System.lineSeparator());
+            }
+            System.out.println("input saved to file " + filePath);
+        }catch (IOException e){
+            System.err.println("error handling file " + e.getMessage());
+        }
+
     }
 }
